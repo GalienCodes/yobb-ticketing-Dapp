d34d03f962943f41d64aaf54a93896f041925b51
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2023-07-02 02:09:21 +0400

    Improvements and cleanup

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -6,9 +6,19 @@ pragma solidity ^0.8.0;
 /// @notice This contract allows event organizers to create events and sell tickets to users
 /// @dev This contract is written in Solidity version 0.8.0
 contract Ticketing {
-
-    event EventCreated(address indexed owner, uint256 indexed eventId, string name, uint256 VipticketPrice, uint256 SilverticketPrice, uint eventDate);
-    event TicketBought(address indexed buyer, string category, uint256 ticketPrice );
+    event EventCreated(
+        address indexed owner,
+        uint256 indexed eventId,
+        string name,
+        uint256 VipticketPrice,
+        uint256 SilverticketPrice,
+        uint eventDate
+    );
+    event TicketBought(
+        address indexed buyer,
+        string category,
+        uint256 ticketPrice
+    );
 
     /// @notice Struct for storing information about a ticket
     struct Ticket {
@@ -19,9 +29,9 @@ contract Ticketing {
         string category;
         string eventName;
         uint256 eventDate;
-        string eventVenue;  
+        string eventVenue;
     }
-    
+
     /// @notice Struct for storing information about an event
     struct Event {
         uint256 eventId;
@@ -35,7 +45,7 @@ contract Ticketing {
         uint256 sellingDuration;
         string eventName;
         uint256 eventDate;
-        string eventVenue;  
+        string eventVenue;
         Ticket[] vipTickets;
         Ticket[] silverTickets;
     }
@@ -47,19 +57,13 @@ contract Ticketing {
     }
 
     /// @notice Mapping of event IDs to events
-    mapping(uint256 => Event) public events;
+    mapping(uint256 => Event) private events;
 
     /// @notice Mapping of user addresses to the number of orders they've made
     mapping(address => uint256) public orderCount;
 
     /// @notice Mapping of user addresses to their orders
     mapping(address => mapping(uint256 => MyOrder)) public myOrders;
-
-    /// @notice Mapping of user addresses to the number of events they've created
-    mapping(address => uint256) public myEventCount;
-
-    /// @notice Mapping of user addresses to their events
-    mapping(address => mapping(uint256 => Event)) public myEvents;
 
     /// @notice Total number of events created
     uint256 public numEvents;
@@ -75,10 +79,12 @@ contract Ticketing {
         uint256 _numSilverTickets,
         uint _vipTicketPrice,
         uint _silverTicketPrice,
-        string memory _eventName,
+        string calldata _eventName,
         uint256 _eventDate,
-        string memory _eventVenue
+        string calldata _eventVenue
     ) public {
+        require(bytes(_eventName).length > 0, "Empty event name");
+        require(bytes(_eventVenue).length > 0, "Empty event venue");
         Event storage newEvent = events[numEvents];
         newEvent.eventId = numEvents;
         newEvent.owner = msg.sender;
@@ -89,67 +95,81 @@ contract Ticketing {
         newEvent.sellingDuration = block.timestamp + SELLINGDURATION;
         newEvent.eventName = _eventName;
         newEvent.eventDate = _eventDate;
-        newEvent.eventVenue = _eventVenue;  
-        
+        newEvent.eventVenue = _eventVenue;
+
         for (uint256 i = 0; i < _numVipTickets; i++) {
             Ticket memory ticket = Ticket({
-                ticketId:tickets_counter,
+                ticketId: tickets_counter,
                 isSold: false,
                 eventId: numEvents,
                 price: _vipTicketPrice,
                 category: "VIP",
                 eventName: _eventName,
                 eventDate: _eventDate,
-                eventVenue:_eventVenue
-
+                eventVenue: _eventVenue
             });
             newEvent.vipTickets.push(ticket);
             tickets_counter++;
-
         }
         for (uint256 i = 0; i < _numSilverTickets; i++) {
-            newEvent.silverTickets.push(Ticket({
-                ticketId: tickets_counter,
-                isSold: false,
-                eventId: numEvents,
-                price: _silverTicketPrice,
-                category: "Silver",
-                eventName: _eventName,
-                eventDate: _eventDate,
-                eventVenue:_eventVenue
-
-            }));
+            newEvent.silverTickets.push(
+                Ticket({
+                    ticketId: tickets_counter,
+                    isSold: false,
+                    eventId: numEvents,
+                    price: _silverTicketPrice,
+                    category: "Silver",
+                    eventName: _eventName,
+                    eventDate: _eventDate,
+                    eventVenue: _eventVenue
+                })
+            );
             tickets_counter++;
         }
         numEvents++;
-        // Add order for user
-        myEventCount[msg.sender]++; // <-- Order ID
-        myEvents[msg.sender][myEventCount[msg.sender]] = newEvent;
-    
-         emit EventCreated(msg.sender, newEvent.eventId, newEvent.eventName, newEvent.vipTicketPrice, newEvent.silverTicketPrice, _eventDate);
-   
+
+        emit EventCreated(
+            msg.sender,
+            newEvent.eventId,
+            newEvent.eventName,
+            newEvent.vipTicketPrice,
+            newEvent.silverTicketPrice,
+            _eventDate
+        );
     }
+
     /// @notice Allows a user to buy a ticket for an event
-    function buyTicket(uint256 _eventId, string memory _category) public payable{
+    function buyTicket(
+        uint256 _eventId,
+        string memory _category
+    ) public payable {
         Event storage eventToBuy = events[_eventId];
-        Ticket[] storage ticketsToBuy;
+        Ticket storage ticketToBuy;
         uint256 numTicketsSold;
         uint256 ticketPrice;
 
-        require(block.timestamp <= eventToBuy.sellingDuration, "Ticket Selling Duration has passed");
+        require(
+            block.timestamp <= eventToBuy.sellingDuration,
+            "Ticket Selling Duration has passed"
+        );
 
-        
-
-        if (keccak256(bytes(_category)) == keccak256(bytes("VIP"))) {
-            require(eventToBuy.vipSold < eventToBuy.numVipTickets, "All VIP tickets are sold out");
-            ticketsToBuy = eventToBuy.vipTickets;
+        bytes32 category = keccak256(bytes(_category));
+        if (category == keccak256(bytes("VIP"))) {
+            require(
+                eventToBuy.vipSold < eventToBuy.numVipTickets,
+                "All VIP tickets are sold out"
+            );
             numTicketsSold = eventToBuy.vipSold;
+            ticketToBuy = eventToBuy.vipTickets[numTicketsSold];
             ticketPrice = eventToBuy.vipTicketPrice;
             eventToBuy.vipSold++;
-        } else if (keccak256(bytes(_category)) == keccak256(bytes("Silver"))) {
-            require(eventToBuy.silverSold < eventToBuy.numSilverTickets, "All Silver tickets are sold out");
-            ticketsToBuy = eventToBuy.silverTickets;
+        } else if (category == keccak256(bytes("Silver"))) {
+            require(
+                eventToBuy.silverSold < eventToBuy.numSilverTickets,
+                "All Silver tickets are sold out"
+            );
             numTicketsSold = eventToBuy.silverSold;
+            ticketToBuy = eventToBuy.silverTickets[numTicketsSold];
             ticketPrice = eventToBuy.silverTicketPrice;
             eventToBuy.silverSold++;
         } else {
@@ -157,36 +177,68 @@ contract Ticketing {
         }
 
         require(msg.value == ticketPrice, "Incorrect amount sent");
-        payable(eventToBuy.owner).transfer(ticketPrice);
-        Ticket storage ticketToBuy = ticketsToBuy[numTicketsSold];
         require(!ticketToBuy.isSold, "Ticket already sold");
         ticketToBuy.isSold = true;
-        // Create order
-        MyOrder memory order = MyOrder(block.timestamp, ticketToBuy);
-        // Add order for user
+        // Increment order count for user
         orderCount[msg.sender]++; // <-- Order ID
-        myOrders[msg.sender][orderCount[msg.sender]] = order;
-        emit TicketBought(msg.sender, _category, ticketPrice );
+        // Create order
+        myOrders[msg.sender][orderCount[msg.sender]] = MyOrder(
+            block.timestamp,
+            ticketToBuy
+        );
+        (bool success, ) = payable(eventToBuy.owner).call{value: ticketPrice}(
+            ""
+        );
+        require(success, "payment failed");
+        emit TicketBought(msg.sender, _category, ticketPrice);
     }
 
- 
-    function getEvent(uint256 _eventId) public view returns (address eventOwner, uint256 eventId, uint256 numVipTickets, uint256 numSilverTickets, uint256 vipSold, uint256 silverSold, uint256 sellingDuration, string memory eventName, string memory _eventVenue, uint256 eventDate, Ticket[] memory vipTickets, Ticket[] memory silverTickets) {
+    function getEvent(
+        uint256 _eventId
+    )
+        public
+        view
+        returns (
+            address eventOwner,
+            uint256 eventId,
+            uint256 numVipTickets,
+            uint256 numSilverTickets,
+            uint256 vipSold,
+            uint256 silverSold,
+            uint256 sellingDuration,
+            string memory eventName,
+            string memory _eventVenue,
+            uint256 eventDate,
+            Ticket[] memory vipTickets,
+            Ticket[] memory silverTickets
+        )
+    {
         Event storage eventToGet = events[_eventId];
-        return (eventToGet.owner, eventToGet.eventId, eventToGet.numVipTickets, eventToGet.numSilverTickets, eventToGet.vipSold, eventToGet.silverSold, eventToGet.sellingDuration, eventToGet.eventName,eventToGet.eventVenue, eventToGet.eventDate, eventToGet.vipTickets, eventToGet.silverTickets);
+        return (
+            eventToGet.owner, 
+            eventToGet.eventId,
+            eventToGet.numVipTickets,
+            eventToGet.numSilverTickets,
+            eventToGet.vipSold,
+            eventToGet.silverSold,
+            eventToGet.sellingDuration,
+            eventToGet.eventName,
+            eventToGet.eventVenue,
+            eventToGet.eventDate,
+            eventToGet.vipTickets,
+            eventToGet.silverTickets
+        );
     }
 
     /**
-    * @dev Returns an array of all events stored in the contract.
-    * @return An array of Event structs.
-    */
+     * @dev Returns an array of all events stored in the contract.
+     * @return An array of Event structs.
+     */
     function getAllEvents() public view returns (Event[] memory) {
         Event[] memory allEvents = new Event[](numEvents);
-        uint256 count = 0;
         for (uint256 i = 0; i < numEvents; i++) {
-                allEvents[count] = events[i];
-                count++;
-            }
+            allEvents[i] = events[i];
+        }
         return allEvents;
     }
-
 }
