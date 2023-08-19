0c9da97ec384a2b937ee5fe8f018b8999caaa109
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-08-19 15:27:26 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="mx-4 sm:mx-6 lg:mx-0  flex flex-col md:flex-row justify-center md:justify-between">
           <h2 className='text-center sm:text-center'> &copy; {date} Yob  </h2>
           <a href="https://www.linkedin.com/in/muhindo-galien/" target='_blank'>
-            <h4 className='text-center cursor-pointer '>Designed By Galien.eth </h4>
+            <h4 className='text-center cursor-pointer '>Designed By Galien_Dev </h4>
           </a>
       </div>
     </div>
